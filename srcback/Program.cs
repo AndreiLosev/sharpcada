a42dab66f842da5fe2df8641228ddc08e7a1e096
Losev Andrei <losev@aheadworks.com>
2023-02-19 14:56:04 +0300

    add app.env and bin/logs

--- srcback/Program.cs
+++ srcback/Program.cs
@@ -1,6 +1,27 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/get", () => "Hello World!");
+var db = 0;
 
-app.Run("http://0.0.0.0:6542");
+app.MapGet("/get", (IConfiguration appConfig) => 
+{
+    db += 1;
+    return $"Hello World! => {db}";
+});
+
+
+RunApp(app);
+
+static void RunApp(WebApplication app)
+{
+    var appConfig = app.Services.GetService<IConfiguration>();
+
+    if (appConfig is null)
+    {
+        throw new Exception();
+    }
+
+    var appPort = appConfig["APP_PORT"];
+    app.Run($"http://0.0.0.0:{appPort}");
+}
+
